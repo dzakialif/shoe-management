1b8043960e57978bb9078f3336d8bca53574dea2
Dzaki <dzakialif912@gmail.com>
2026-08-17 02:57:28 +0700

    feat: regiter endpoint to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ShoeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
+});
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('brands', BrandController::class);
+    Route::apiResource('shoes', ShoeController::class);
 });
